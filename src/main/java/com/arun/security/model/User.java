e01f5a4789884bfd991c00c192237c02128ffbdd
arunbharath1082 <arunbarath2000@gmail.com>
2024-09-02 22:43:25 +0530

    spring security

--- src/main/java/com/arun/security/model/User.java
+++ src/main/java/com/arun/security/model/User.java
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Repository;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+
     private int id;
     private String name;
     private String email;
